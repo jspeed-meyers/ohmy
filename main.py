e79e588a2aed2c459a8a6199bd554c7e736b4558
John Speed Meyers <jsmeyers@chainguard.dev>
2025-02-01 14:10:15 -0500

    Refactor to use a separate python functions file
    Signed-off-by: John Speed Meyers <jsmeyers@chainguard.dev>

--- main.py
+++ main.py
@@ -1,67 +1,14 @@
-import requests
+import argparse
 
-def get_latest_pypi_version(package_name):
-    url = f"https://pypi.org/pypi/{package_name}/json"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data["info"]["version"]
-    else:
-        print(f"Error: {response.status_code}")
-        return None
+import python
 
-def get_pypi_files(package_name, version):
-    url = f"https://pypi.org/pypi/{package_name}/{version}/json"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        files = data.get("urls", [])  # "urls" contains the list of uploaded files
-        return files
-    else:
-        print(f"Error: {response.status_code}")
-        return None
+def parse_cli():
+    parser = argparse.ArgumentParser(description='Assess the trusted build score of your Python packages.')
+    parser.add_argument('-f', '--file', type=str, help='The name of the dependency file to be ingested.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Option to print verbose output.')
+    return parser.parse_args()
 
-def retrieve_trusted_build_json(package_name, version, filename):
-    url = f"https://pypi.org/integrity/{package_name}/{version}/{filename}/provenance"
-    headers = {
-        "Accept": "application/vnd.pypi.integrity.v1+json"
-    }
-    response = requests.get(url, headers=headers)
-    return response
-
-def check_trusted_build_status(trusted_build_json):
-    if trusted_build_json.status_code == 200:
-        return True
-    else:
-        return False
-
-def get_trusted_build_determination(package_name):
-    latest_version = get_latest_pypi_version(package_name)
-    files = get_pypi_files(package_name, latest_version)
-    filename = files[0]['filename']
-    test_json = retrieve_trusted_build_json(package_name, latest_version, filename)
-    return check_trusted_build_status(test_json)
-
-def get_package_names(requirements_file):
-    with open(requirements_file, "r") as file:
-        packages = [
-            line.split("==")[0].strip().split(">=")[0].split("<=")[0]
-            for line in file
-            if line.strip() and not line.startswith("#")
-        ]
-    return packages
-
-
-if __name__ == "__main__":
-    requirements_file = "requirements.txt"
-    pkgs = get_package_names(requirements_file)
-    
-    package_status = {}
-    for pkg in pkgs:
-        package_status[pkg] = get_trusted_build_determination(pkg)
-
+def print_output(package_status):
     cnt = 0
     for pkg in package_status.keys():
         if package_status[pkg]:
@@ -69,12 +16,29 @@ if __name__ == "__main__":
     
     print("")
     print("TRUSTED BUILD STATUS")
-    print(f"PyPI: {cnt / len(package_status) * 100}%")
-    print("chibbies: 100%")
+    print("--------------------")
+    print(f"PyPI     | {round(cnt / len(package_status) * 100)}%")
+    print("--------------------")
+    print("chibbies | 100%")
     print("")
-    print("PyPI Details")
-    for pkg in package_status.keys():
-        if package_status[pkg]:
-            print(f"{pkg}: Trusted")
-        else:
-            print(f"{pkg}: Untrusted")
+
+    if args.verbose:
+        print("PyPI Details")
+        print("============")
+        for pkg in package_status.keys():
+            if package_status[pkg]:
+                print(f"{pkg}: Trusted")
+            else:
+                print(f"{pkg}: Untrusted")
+
+if __name__ == "__main__":
+
+    args = parse_cli()
+
+    requirements_file = args.file
+
+    pkgs = python.get_package_names(requirements_file)
+    
+    package_status = python.get_python_package_status(pkgs) 
+
+    print_output(package_status)
---
+++ python.py
@@ -0,0 +1,60 @@
+import requests
+
+def get_latest_pypi_version(package_name):
+    url = f"https://pypi.org/pypi/{package_name}/json"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data["info"]["version"]
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def get_pypi_files(package_name, version):
+    url = f"https://pypi.org/pypi/{package_name}/{version}/json"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        files = data.get("urls", [])  # "urls" contains the list of uploaded files
+        return files
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def retrieve_trusted_build_json(package_name, version, filename):
+    url = f"https://pypi.org/integrity/{package_name}/{version}/{filename}/provenance"
+    headers = {
+        "Accept": "application/vnd.pypi.integrity.v1+json"
+    }
+    response = requests.get(url, headers=headers)
+    return response
+
+def check_trusted_build_status(trusted_build_json):
+    if trusted_build_json.status_code == 200:
+        return True
+    else:
+        return False
+
+def get_trusted_build_determination(package_name):
+    latest_version = get_latest_pypi_version(package_name)
+    files = get_pypi_files(package_name, latest_version)
+    filename = files[0]['filename']
+    test_json = retrieve_trusted_build_json(package_name, latest_version, filename)
+    return check_trusted_build_status(test_json)
+
+def get_package_names(requirements_file):
+    with open(requirements_file, "r") as file:
+        packages = [
+            line.split("==")[0].strip().split(">=")[0].split("<=")[0]
+            for line in file
+            if line.strip() and not line.startswith("#")
+        ]
+    return packages
+
+def get_python_package_status(pkgs):
+    package_status = {}
+    for pkg in pkgs:
+        package_status[pkg] = get_trusted_build_determination(pkg)
+    return package_status
